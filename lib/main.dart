8e93846d5ae331646e463637b0860b3241252410
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-08-08 11:46:12 +0100

    Package Add

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Spoonacular',
+        title: 'Movie DB APP',
         debugShowCheckedModeBanner: false,
         initialRoute: '.',
         routes: {'/': (_) => HomePage()});
