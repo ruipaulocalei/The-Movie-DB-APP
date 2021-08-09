bc1519b55c59ad1b8e0df89a0ca87e703d750de6
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-08-09 11:54:49 +0100

    Search Delegate

---
+++ lib/ui/search/search_delegate.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MovieSearch extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [IconButton(onPressed: () {}, icon: Icon(Icons.clear))];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {},
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container();
+  }
+}
